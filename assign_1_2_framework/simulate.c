c3ad95bf5d2f9a38839142ed0aedcff244e9f66d
willemebe.vd.spek@gmail.com <willemebe.vd.spek@gmail.com>
2018-11-08 18:01:49 +0100

    updated simulate.c, now works for n threads

--- assign_1_2_framework/simulate.c
+++ assign_1_2_framework/simulate.c
@@ -27,18 +27,20 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
     float wave_param = 0.15;
+    i_max -= 5;
 
-    #pragma omp parallel for private(t, i)
+    #pragma omp parallel for num_threads(num_threads)
     for (int t = 0; t < t_max; t++) {
         current_array = next_array;
         old_array = current_array;
         next_array[0] = current_array[0];
         next_array[i_max] = current_array[i_max];
 
-        for (int i = 1; i < i_max - 1; i++) {
+        for (int i = 1; i < i_max - 1; i += 4) {
             next_array[i] = 2 * current_array[i] - old_array[i] + (wave_param *
             (current_array[i - 1] - (2 *
             current_array[i] - current_array[i + 1])));
+             //printf("t = %d, i= %d, threadId = %d \n", t, i, omp_get_thread_num());
         }
     }
 
