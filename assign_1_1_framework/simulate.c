fba23b553f899a4a2cd367cae1fc9b59c649ffe8
Jorik <jorik.smorenburg@gmail.com>
2018-11-09 13:02:12 +0100

    Pthread assignment 1.1

--- assign_1_1_framework/simulate.c
+++ assign_1_1_framework/simulate.c
@@ -6,14 +6,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "simulate.h"
 
 
 /* Add any global variables you may need. */
+typedef struct{
+    int start;
+    int end;
+    double *old_array;
+    double *current_array;
+    double *next_array;
+} update_args;
+
+#define WAVE_PARAM 0.15;
 
 
 /* Add any functions you may need (like a worker) here. */
+void *update_wave (void *p) {
+    update_args *args = (update_args *) p;
+    int start = args->start;
+    int end = args->end;
+    double *old_array = args->old_array;
+    double *current_array = args->current_array;
+    double *next_array = args->next_array;
+
+    for (int i = start; i < end; i ++) {
+        next_array[i] = 2 * current_array[i] - old_array[i] + 0.15 *
+        (current_array[i - 1] - 2 * current_array[i] - current_array[i +  1]);
+    }
+
+    return NULL;
+}
 
 
 /*
@@ -38,6 +63,43 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
      * After each timestep, you should swap the buffers around. Watch out none
      * of the threads actually use the buffers at that time.
      */
+
+    pthread_t thread_ids[num_threads];
+
+    int iterations_per_thread = (i_max - 2) / num_threads;
+    int rem_iterations = (i_max - 2) % num_threads;
+
+    for (int t = 0; t < t_max; t++) {
+        // update array per timepoint
+        current_array = next_array;
+        old_array = current_array;
+        // initiliaze first and last wavepoint
+        next_array[0] = current_array[0];
+        next_array[i_max] = current_array[i_max];
+
+
+        for (int i = 0; i < num_threads; i++) {
+            update_args *args = (update_args*) malloc(sizeof(update_args));
+            if (!args) {
+                printf("Rip world\n");
+            }
+
+            int iterations = iterations_per_thread + rem_iterations / (i + 1);
+            int start = iterations * i;
+            int end = start + iterations;
+
+            args->start = start;
+            args->end = end;
+            args->old_array = old_array;
+            args->current_array = current_array;
+            args->next_array = next_array;
+
+            pthread_create (&thread_ids[i],
+                            NULL,
+                            &update_wave,
+                            (void *)&args);
+        }
+    }
 
 
     /* You should return a pointer to the array with the final results. */
